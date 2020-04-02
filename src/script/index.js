2f79475fd8415c8fc9d41d6ff28ce5a2d66977db
Sergey <sergejleontev111@gmail.com>
2020-04-03 02:58:34 +0300

    some change

--- src/script/index.js
+++ src/script/index.js
@@ -10,13 +10,15 @@ const ctx = canvas.getContext('2d')
 
 document.querySelector('.btn').addEventListener('click', e => {
     clearTimeout(timer)
+    clearTimeout(timer_2)
+    clearTimeout(timer_3)
 })
 // ctx.beginPath()
 let x = 0
-let timer
+let timer, timer_2, timer_3
 
 function drawSin() {
-    let y = 200 + 100*Math.sin(x/24)
+    let y = 400 - 150 + 300*Math.cos(x/24 + Math.PI/3)
     x = x + 0.1
     ctx.fillStyle = 'red'
     ctx.fillRect(x,y,2,2)
@@ -24,12 +26,21 @@ function drawSin() {
 }
 
 function drawSin_2() {
-    let y = 200 + 100*Math.sin(x/4)
+    let y = 400 + 50*Math.cos(x/24)
+    x = x + 0.1
+    ctx.fillStyle = 'black'
+    ctx.fillRect(x,y,2,2)
+    timer_2 = setTimeout(drawSin_2, 10)
+}
+
+function drawSin_3() {
+    let y = 200 + 50*Math.cos(x/48)
     x = x + 0.1
     ctx.fillStyle = 'blue'
     ctx.fillRect(x,y,2,2)
-    timer = setTimeout(drawSin_2, 10)
+    timer_3 = setTimeout(drawSin_3, 10)
 }
 
 drawSin()
 drawSin_2()
+// drawSin_3()
